0e2f35f33df68331545c2b719de938ed2dc28129
pelayo <pelayopc@gmail.com>
2020-07-17 09:34:44 +0200

    Rest Server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,18 @@
 // =====================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ENTORNO
+
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+// BD
+
+let urlDB;
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cafe";
+} else {
+  urlDB =
+    "mongodb+srv://strider:ofDJ82p88AjRYFn2@cluster0.wh1uz.mongodb.net/cafe";
+}
+process.env.URLDB = urlDB;
